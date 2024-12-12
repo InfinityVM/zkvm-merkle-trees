6c9964e25bc860598c3f0aa15ade906cef36d8c1
Avi Dessauer <avi.the.coder@gmail.com>
2024-12-11 20:38:54 -0800

    Implement remove and balancing operations & some tests

--- zkvm-merkle-btree/src/node.rs
+++ zkvm-merkle-btree/src/node.rs
@@ -1,3 +1,6 @@
+use core::panic;
+use std::iter;
+
 use alloc::sync::Arc;
 
 use arrayvec::ArrayVec;
@@ -5,7 +8,7 @@ use arrayvec::ArrayVec;
 use crate::store::Idx;
 
 /// TODO make it configurable
-const BTREE_ORDER: usize = 6;
+const BTREE_ORDER: usize = 2;
 pub const EMPTY_TREE_ROOT_HASH: [u8; 32] = [0; 32];
 
 pub type NodeHash = [u8; 32];
@@ -30,9 +33,13 @@ pub struct Node<K, V> {
     pub children: ArrayVec<NodeRef<K, V>, { BTREE_ORDER * 2 }>,
 }
 
-impl<K, V> Node<K, V> {
+impl<K: Ord + Clone, V: Clone> Node<K, V> {
     pub fn is_full(&self) -> bool {
         self.keys.len() == BTREE_ORDER * 2 - 1
+    }
+
+    pub fn is_to_small(&self) -> bool {
+        self.keys.len() < Self::min_keys()
     }
 
     pub const fn max_children() -> usize {
@@ -50,27 +57,161 @@ impl<K, V> Node<K, V> {
     pub const fn min_keys() -> usize {
         BTREE_ORDER - 1
     }
-}
 
-impl<K: Clone + Ord + std::fmt::Debug, V: std::fmt::Debug + Clone> Node<K, V> {
-    #[track_caller]
-    pub fn insert(&mut self, idx: usize, key: K, child: NodeRef<K, V>) {
-        self.keys.insert(idx, key);
-        self.children.insert(idx + 1, child);
+    /// Try to merge or balance the node with a sibling.
+    /// Returns Ok(()) if the node was merged or balanced.
+    /// Returns Err(()) if the both siblings are stored nodes.
+    ///
+    /// This method is left biased, it will try to merge or balance with the left sibling first.
+    /// This method will skip a stored sibling to avoid bloating the snapshot.
+    /// The order of attempts is left merge, right merge, left balance, right balance.
+    #[allow(clippy::result_unit_err)]
+    pub fn merge_or_balance(&mut self, underflow_idx: usize) -> Result<(), ()> {
+        let children_keys_len = self.children[underflow_idx].node().unwrap().keys.len();
+        debug_assert!(children_keys_len < Self::min_keys());
 
-        if cfg!(debug_assertions) {
-            self.assert_invariants();
+        let (left, underflowed, right) = if underflow_idx == 0 {
+            let Some([underflow_idx, right, ..]) = self.children.get_mut(underflow_idx..) else {
+                return Ok(());
+            };
+            (&mut NodeRef::Null, underflow_idx, right)
+        } else if underflow_idx + 1 == self.children.len() {
+            let Some([left, underflow_idx, ..]) = self.children.get_mut(underflow_idx - 1..) else {
+                return Ok(());
+            };
+            (left, underflow_idx, &mut NodeRef::Null)
+        } else {
+            let Some([left, underflow_idx, right, ..]) = self.children.get_mut(underflow_idx - 1..)
+            else {
+                return Ok(());
+            };
+
+            (left, underflow_idx, right)
+        };
+
+        match (left, underflowed, right) {
+            (NodeRef::Node(left_arc), NodeRef::Node(underflow_arc), _)
+                if left_arc.can_be_merged(underflow_arc) =>
+            {
+                let left_node = Arc::make_mut(left_arc);
+                let underflow_node = Arc::make_mut(underflow_arc);
+                let middle_key = self.keys.remove(underflow_idx - 1);
+                left_node.merge(middle_key, underflow_node);
+                self.children.remove(underflow_idx);
+                Ok(())
+            }
+
+            (_, NodeRef::Node(underflow_arc), NodeRef::Node(right_arc))
+                if underflow_arc.can_be_merged(right_arc) =>
+            {
+                let underflow_node = Arc::make_mut(underflow_arc);
+                let right_node = Arc::make_mut(right_arc);
+                let middle_key = self.keys.remove(underflow_idx);
+                underflow_node.merge(middle_key, right_node);
+                self.children.remove(underflow_idx + 1);
+                Ok(())
+            }
+
+            (NodeRef::Node(left_arc), NodeRef::Node(underflow_arc), _) => {
+                debug_assert!(left_arc.keys.len() > Self::min_keys());
+
+                let left_node = Arc::make_mut(left_arc);
+                let underflow_node = Arc::make_mut(underflow_arc);
+
+                let middle_key = self.keys.get_mut(underflow_idx - 1).unwrap();
+                left_node.balance(middle_key, underflow_node);
+
+                Ok(())
+            }
+
+            (_, NodeRef::Node(underflow_arc), NodeRef::Node(right_arc)) => {
+                debug_assert!(right_arc.keys.len() > Self::min_keys());
+
+                let underflow_node = Arc::make_mut(underflow_arc);
+                let right_node = Arc::make_mut(right_arc);
+
+                let middle_key = self.keys.get_mut(underflow_idx).unwrap();
+                underflow_node.balance(middle_key, right_node);
+
+                Ok(())
+            }
+
+            // FIXME
+            // (NodeRef::Leaf())
+            (NodeRef::Stored(_), _, NodeRef::Stored(_)) => Err(()),
+
+            _ => {
+                unreachable!("merge_or_balance called on node with one child");
+            }
         }
+    }
+
+    pub fn balance(&mut self, middle_key: &mut K, right_node: &mut Self) {
+        // assert that we could not merge
+        // one is the middle key from the parent
+        debug_assert!(self.keys.len() + 1 + right_node.keys.len() > Self::max_keys());
+        debug_assert!(self.children.len() + right_node.children.len() > Self::max_children());
+
+        let child_count = self.children.len() + right_node.children.len();
+        let left_count = child_count / 2;
+        // This is not the most efficient way of doing this, but we need to replace the ArrayVec anyway.
+        let mut children = self.children.drain(..).chain(right_node.children.drain(..));
+        let mut keys = self
+            .keys
+            .drain(..)
+            .chain(iter::once(middle_key.clone()))
+            .chain(right_node.keys.drain(..));
+
+        let left_keys = (&mut keys).take(left_count - 1).collect();
+        *middle_key = keys.next().unwrap();
+        let right_key = keys.collect();
+        let left_children = (&mut children).take(left_count).collect();
+        let right_children = children.collect();
+
+        self.keys = left_keys;
+        self.children = left_children;
+
+        right_node.keys = right_key;
+        right_node.children = right_children;
+
+        self.assert_invariants();
+        right_node.assert_invariants();
+    }
+
+    pub fn merge(&mut self, middle_key: K, right_node: &mut Self) {
+        let key_count = self.keys.len() + 1 + right_node.keys.len();
+        let child_count = self.children.len() + right_node.children.len();
+        debug_assert!(key_count <= Self::max_keys());
+        debug_assert!(child_count <= Self::max_children());
+        debug_assert!(key_count == child_count - 1);
+
+        self.keys.push(middle_key);
+        self.keys.extend(right_node.keys.drain(..));
+
+        self.children.extend(right_node.children.drain(..));
+
+        self.assert_invariants();
+    }
+
+    #[inline(always)]
+    fn can_be_merged(&self, right_node: &Self) -> bool {
+        #[cfg(debug_assertions)]
+        {
+            match (self.keys.as_slice(), right_node.keys.as_slice()) {
+                ([.., left_last], [right_first, ..]) => assert!(left_last < right_first),
+                ([], _) => assert!(self.children.len() == 1),
+                (_, []) => assert!(right_node.children.len() == 1),
+            };
+        }
+
+        // We use less than because the middle key from the parent also needs to be added.
+        self.keys.len() + right_node.keys.len() < Self::max_keys()
     }
 
     #[track_caller]
     pub fn assert_keys_sorted(&self) {
         for i in 1..self.keys.len() {
-            assert!(
-                self.keys[i - 1] < self.keys[i],
-                "keys are not sorted: {:?}",
-                self.keys
-            );
+            assert!(self.keys[i - 1] < self.keys[i],);
         }
     }
 
@@ -80,26 +221,12 @@ impl<K: Clone + Ord + std::fmt::Debug, V: std::fmt::Debug + Clone> Node<K, V> {
         for (i, child) in self.children.iter().enumerate() {
             match child {
                 NodeRef::Leaf(leaf) => {
-                    let _ = self.keys.get(i).map(|key| {
-                        assert!(
-                            *key >= leaf.key,
-                            "Assertion failed: key: {:?} >= leaf.key: {:?} in node {:?}",
-                            key,
-                            leaf.key,
-                            self
-                        )
-                    });
+                    let _ = self.keys.get(i).map(|key| assert!(*key >= leaf.key,));
 
                     if let Some(prior_key) = prior_child_key {
-                        assert!(
-                            prior_key < leaf.key,
-                            "prior_key: {:?}, leaf: {:?}, children: {:?}",
-                            prior_key,
-                            leaf.key,
-                            self.children
-                        );
+                        assert!(prior_key < &leaf.key);
                     }
-                    prior_child_key = Some(leaf.key.clone());
+                    prior_child_key = Some(&leaf.key);
                 }
                 NodeRef::Node(node) => {
                     let _ = self
@@ -107,11 +234,12 @@ impl<K: Clone + Ord + std::fmt::Debug, V: std::fmt::Debug + Clone> Node<K, V> {
                         .get(i)
                         .map(|key| assert!(key >= node.keys.last().unwrap()));
 
-                    let child_key = node.keys.last().unwrap().clone();
-                    if let Some(prior_key) = prior_child_key {
-                        assert!(prior_key < child_key)
+                    if let Some(child_key) = node.keys.last() {
+                        if let Some(prior_key) = prior_child_key {
+                            assert!(prior_key < child_key);
+                        }
+                        prior_child_key = Some(child_key);
                     }
-                    prior_child_key = Some(child_key);
                 }
                 _ => {}
             }
@@ -119,11 +247,7 @@ impl<K: Clone + Ord + std::fmt::Debug, V: std::fmt::Debug + Clone> Node<K, V> {
     }
 
     #[track_caller]
-    pub fn assert_invariants(&self)
-    where
-        K: Ord + Clone + std::fmt::Debug,
-        V: Clone + std::fmt::Debug,
-    {
+    pub fn assert_invariants(&self) {
         let all_children_are_leafs = self
             .children
             .iter()
@@ -155,6 +279,56 @@ pub struct Leaf<K, V> {
 pub enum NodeOrLeaf<K, V> {
     Node(Arc<Node<K, V>>),
     Leaf(Arc<Leaf<K, V>>),
+}
+
+impl<K, V> NodeOrLeaf<K, V> {
+    pub fn node(&self) -> Option<&Arc<Node<K, V>>> {
+        match self {
+            NodeOrLeaf::Node(node) => Some(node),
+            _ => None,
+        }
+    }
+
+    pub fn leaf(&self) -> Option<&Arc<Leaf<K, V>>> {
+        match self {
+            NodeOrLeaf::Leaf(leaf) => Some(leaf),
+            _ => None,
+        }
+    }
+}
+
+impl<K, V> TryFrom<NodeOrLeaf<K, V>> for Arc<Node<K, V>> {
+    type Error = ();
+
+    fn try_from(node_or_leaf: NodeOrLeaf<K, V>) -> Result<Self, Self::Error> {
+        match node_or_leaf {
+            NodeOrLeaf::Node(node) => Ok(node),
+            NodeOrLeaf::Leaf(_) => Err(()),
+        }
+    }
+}
+
+impl<K, V> TryFrom<NodeOrLeaf<K, V>> for Arc<Leaf<K, V>> {
+    type Error = ();
+
+    fn try_from(node_or_leaf: NodeOrLeaf<K, V>) -> Result<Self, Self::Error> {
+        match node_or_leaf {
+            NodeOrLeaf::Node(_) => Err(()),
+            NodeOrLeaf::Leaf(leaf) => Ok(leaf),
+        }
+    }
+}
+
+impl<K, V> From<Arc<Leaf<K, V>>> for NodeOrLeaf<K, V> {
+    fn from(leaf: Arc<Leaf<K, V>>) -> Self {
+        NodeOrLeaf::Leaf(leaf)
+    }
+}
+
+impl<K, V> From<Arc<Node<K, V>>> for NodeOrLeaf<K, V> {
+    fn from(node: Arc<Node<K, V>>) -> Self {
+        NodeOrLeaf::Node(node)
+    }
 }
 
 #[derive(Debug, Clone)]
--- zkvm-merkle-btree/src/transaction.rs
+++ zkvm-merkle-btree/src/transaction.rs
@@ -254,7 +254,17 @@ impl<S: Store> MerkleBTreeTxn<S> {
                                     node.keys.insert(idx, new_leaf.key.clone());
                                     node.children.insert(idx + 1, NodeRef::Leaf(new_leaf));
                                 }
+                            } else if new_leaf.key < old_key {
+                                right_node.keys.insert(
+                                    idx - Node::<S::Key, S::Value>::min_children(),
+                                    old_key,
+                                );
+                                right_node.children.insert(
+                                    idx - Node::<S::Key, S::Value>::min_children(),
+                                    NodeRef::Leaf(new_leaf),
+                                );
                             } else {
+                                debug_assert!(new_leaf.key > old_key);
                                 right_node.keys.insert(
                                     idx - Node::<S::Key, S::Value>::min_children(),
                                     new_leaf.key.clone(),
@@ -311,13 +321,141 @@ impl<S: Store> MerkleBTreeTxn<S> {
         }
     }
 
+    pub fn remove(&mut self, key: &S::Key) -> Result<Option<S::Value>, S::Error> {
+        match Self::remove_inner(&self.data_store, &mut self.current_root, key)? {
+            Remove::NotPresent => Ok(None),
+            Remove::Removed(value) => Ok(Some(value)),
+            Remove::Underflow(value) => {
+                match self.current_root {
+                    // remove_inner removed the single leaf node replacing it with null
+                    NodeRef::Null => Ok(Some(value)),
+                    NodeRef::Leaf(_) => {
+                        unreachable!("Removing a leaf replaces it with null")
+                    }
+                    NodeRef::Stored(_) => {
+                        unreachable!("Stored node would have been replaced with a node or leaf")
+                    }
+                    NodeRef::Node(ref mut node) => {
+                        if node.keys.is_empty() {
+                            debug_assert!(node.children.len() == 1);
+                            self.current_root = Arc::make_mut(node).children.pop().unwrap();
+                        }
+
+                        Ok(Some(value))
+                    }
+                }
+            }
+        }
+    }
+
+    fn remove_inner(
+        data_store: &S,
+        node: &mut NodeRef<S::Key, S::Value>,
+        key: &S::Key,
+    ) -> Result<Remove<S>, S::Error> {
+        match node {
+            NodeRef::Stored(idx) => {
+                *node = NodeRef::from(data_store.get(*idx)?);
+
+                // TODO use loop and break
+                Self::remove_inner(data_store, node, key)
+            }
+            NodeRef::Node(node) => {
+                Node::assert_invariants(node);
+                let node = Arc::make_mut(node);
+
+                let idx = match node.keys.binary_search(key) {
+                    Ok(equal_key_idx) => equal_key_idx + 1,
+                    Err(idx) => idx,
+                };
+
+                match Self::remove_inner(data_store, &mut node.children[idx], key)? {
+                    Remove::NotPresent => Ok(Remove::NotPresent),
+                    Remove::Removed(value) => Ok(Remove::Removed(value)),
+                    Remove::Underflow(value) => {
+                        Self::handle_underflow(data_store, node, idx, value)
+                    }
+                }
+            }
+            NodeRef::Leaf(leaf) => {
+                if &leaf.key == key {
+                    // TODO maybe return whole leaf or do somthing more advanced to enforce good usage
+                    let value = leaf.value.clone();
+                    *node = NodeRef::Null;
+                    Ok(Remove::Underflow(value))
+                } else {
+                    Ok(Remove::NotPresent)
+                }
+            }
+            NodeRef::Null => Ok(Remove::NotPresent),
+        }
+    }
+
+    fn handle_underflow(
+        data_store: &S,
+        node: &mut Node<S::Key, S::Value>,
+        idx: usize,
+        value: S::Value,
+    ) -> Result<Remove<S>, S::Error> {
+        match node.children[idx] {
+            NodeRef::Node(_) => {
+                if let Err(()) = node.merge_or_balance(idx) {
+                    if idx == 0 {
+                        let hash_idx = node.children[1].stored().unwrap();
+                        node.children[1] = NodeRef::Node(
+                            data_store
+                                .get(hash_idx)?
+                                .try_into()
+                                .expect("unbalanced node"),
+                        );
+                    } else {
+                        let hash_idx = node.children[idx - 1].stored().unwrap();
+                        node.children[idx - 1] = NodeRef::Node(
+                            data_store
+                                .get(hash_idx)?
+                                .try_into()
+                                .expect("unbalanced node"),
+                        );
+                        // We just added a sibling node to the tree
+                        node.merge_or_balance(idx).unwrap()
+                    }
+                };
+
+                if node.is_to_small() {
+                    Ok(Remove::Underflow(value))
+                } else {
+                    Ok(Remove::Removed(value))
+                }
+            }
+            // A leaf was just removed leaving null
+            NodeRef::Null => {
+                if idx == 0 {
+                    node.keys.remove(idx);
+                } else {
+                    node.keys.remove(idx - 1);
+                }
+
+                node.children.remove(idx);
+
+                if node.keys.len() < Node::<S::Key, S::Value>::min_keys() {
+                    Ok(Remove::Underflow(value))
+                } else {
+                    Ok(Remove::Removed(value))
+                }
+            }
+            _ => {
+                unreachable!("Underflow is only returned from visiting a node or leaf")
+            }
+        }
+    }
+
     /// Prints the whole tree in a pretty format.
     pub fn pretty_print(&self) {
-        self.pretty_print_node(&self.current_root, 0).unwrap();
+        Self::pretty_print_node(&self.data_store, &self.current_root, 0).unwrap();
     }
 
     fn pretty_print_node(
-        &self,
+        data_store: &S,
         node_ref: &NodeRef<S::Key, S::Value>,
         indent: usize,
     ) -> Result<(), S::Error> {
@@ -326,7 +464,7 @@ impl<S: Store> MerkleBTreeTxn<S> {
             NodeRef::Node(node) => {
                 println!("{}Node(keys: {:?})", indent_str, node.keys);
                 for child in &node.children {
-                    self.pretty_print_node(child, indent + 1)?;
+                    Self::pretty_print_node(data_store, child, indent + 1)?;
                 }
             }
             NodeRef::Leaf(leaf) => {
@@ -336,9 +474,9 @@ impl<S: Store> MerkleBTreeTxn<S> {
                 );
             }
             NodeRef::Stored(idx) => {
-                let node_or_leaf = self.data_store.get(*idx)?;
+                let node_or_leaf = data_store.get(*idx)?;
                 let node_ref = NodeRef::from(node_or_leaf);
-                self.pretty_print_node(&node_ref, indent)?;
+                Self::pretty_print_node(data_store, &node_ref, indent)?;
             }
             NodeRef::Null => {
                 println!("{}Null", indent_str);
@@ -359,6 +497,13 @@ enum Insert<S: Store> {
     SplitLeaf {
         new_leaf: Arc<Leaf<S::Key, S::Value>>,
     },
+}
+
+pub enum Remove<S: Store> {
+    NotPresent,
+    Removed(S::Value),
+    /// Removing caused a node to be smaller than the minimum size.
+    Underflow(S::Value),
 }
 
 impl<S: Store> Debug for Insert<S> {
@@ -393,6 +538,7 @@ mod test {
     enum Op {
         Insert(u32, u32),
         Get(u32),
+        Delete(u32),
     }
 
     fn run_operations(operations: Vec<Op>) {
@@ -415,28 +561,39 @@ mod test {
                         "get failed for key: {}, merkle btree returned {:?}, btreemap returned {:?}",
                         k, res_txn, res_std);
                 }
+                Op::Delete(k) => {
+                    let res_txn = txn_btree.remove(&k).unwrap();
+                    let res_std = std_btree.remove(&k);
+                    assert_eq!(res_txn, res_std,
+                        "delete failed for key: {}, merkle btree returned {:?}, btreemap returned {:?}",
+                        k, res_txn, res_std);
+                }
             }
         }
     }
 
     #[test]
-    fn test_merkle_btree_txn_hardcoded_1() {
-        let operations = vec![
-            Op::Insert(1551649896, 0),
-            Op::Insert(1551649897, 0),
-            Op::Insert(0, 0),
-            Op::Insert(0, 0),
-        ];
-        run_operations(operations);
-    }
-    #[test]
-    fn test_merkle_btree_txn_hardcoded_2() {
+    fn test_insert_1() {
         let operations = vec![Op::Insert(0, 0), Op::Insert(1, 0), Op::Insert(2, 0)];
         run_operations(operations);
     }
 
     #[test]
-    fn test_merkle_btree_txn_hardcoded_3() {
+    fn test_insert_2() {
+        let operations = vec![
+            Op::Insert(15, 0),
+            Op::Insert(16, 0),
+            Op::Insert(17, 0),
+            Op::Insert(18, 0),
+            Op::Insert(19, 0),
+            Op::Insert(20, 0),
+            Op::Insert(0, 0),
+        ];
+        run_operations(operations);
+    }
+
+    #[test]
+    fn test_insert_3() {
         let operations = vec![
             Op::Insert(0, 0),
             Op::Insert(1, 0),
@@ -450,15 +607,151 @@ mod test {
     }
 
     #[test]
-    fn test_merkle_btree_txn_minimal_failing() {
+    fn test_insert_duplicate_1() {
         let operations = vec![
-            Op::Insert(15, 0),
-            Op::Insert(16, 0),
-            Op::Insert(17, 0),
-            Op::Insert(18, 0),
-            Op::Insert(19, 0),
-            Op::Insert(20, 0),
+            Op::Insert(1551649896, 0),
+            Op::Insert(1551649897, 0),
             Op::Insert(0, 0),
+            Op::Insert(0, 0), // Duplicate insert
+        ];
+        run_operations(operations);
+    }
+
+    #[test]
+    fn test_insert_delete_1() {
+        let operations = vec![Op::Insert(0, 0), Op::Insert(1, 0), Op::Delete(1)];
+        run_operations(operations);
+    }
+
+    #[test]
+    fn test_insert_delete_get_1() {
+        let operations = vec![
+            Op::Insert(1, 10),
+            Op::Insert(2, 20),
+            Op::Insert(3, 30),
+            Op::Delete(2),
+            Op::Get(2),
+            Op::Delete(1),
+            Op::Delete(3),
+        ];
+        run_operations(operations);
+    }
+
+    #[test]
+    fn test_insert_delete_2() {
+        let operations = vec![
+            Op::Insert(0, 0),
+            Op::Insert(1, 0),
+            Op::Insert(2, 0),
+            Op::Insert(3, 0),
+            Op::Insert(257, 0),
+            Op::Insert(63, 0),
+            Op::Insert(4, 0),
+            Op::Insert(5, 0),
+            Op::Insert(64, 0),
+            Op::Insert(65, 0),
+            Op::Insert(66, 0),
+            Op::Insert(67, 0),
+            Op::Insert(6, 0),
+            Op::Delete(257),
+        ];
+        run_operations(operations);
+    }
+
+    #[test]
+    fn test_insert_delete_get_2() {
+        let operations = vec![
+            Op::Insert(100, 1000),
+            Op::Insert(200, 2000),
+            Op::Insert(300, 3000),
+            Op::Delete(200),
+            Op::Get(200),
+            Op::Insert(400, 4000),
+            Op::Insert(500, 5000),
+            Op::Delete(100),
+            Op::Get(100),
+            Op::Delete(300),
+            Op::Get(300),
+            Op::Delete(400),
+            Op::Get(400),
+            Op::Delete(500),
+            Op::Get(500),
+        ];
+        run_operations(operations);
+    }
+
+    #[test]
+    fn test_minimal_failing_input() {
+        let operations = vec![
+            Op::Insert(887, 0),
+            Op::Insert(0, 0),
+            Op::Insert(1, 0),
+            Op::Insert(119, 0),
+            Op::Insert(2, 0),
+            Op::Delete(887),
+        ];
+        run_operations(operations);
+    }
+
+    #[test]
+    fn test_minimal_failing_input_2() {
+        let operations = vec![
+            Op::Insert(0, 0),
+            Op::Insert(0, 0),
+            Op::Insert(422, 0),
+            Op::Insert(422, 0),
+            Op::Insert(423, 0),
+            Op::Insert(424, 0),
+            Op::Insert(425, 0),
+            Op::Delete(422),
+        ];
+        run_operations(operations);
+    }
+
+    #[test]
+    fn test_minimal_failing_input_3() {
+        let operations = vec![
+            Op::Insert(0, 0),
+            Op::Insert(1, 0),
+            Op::Insert(2, 0),
+            Op::Insert(3, 0),
+            Op::Insert(4, 0),
+            Op::Insert(580, 0),
+            Op::Insert(581, 0),
+            Op::Insert(582, 0),
+            Op::Insert(5, 0),
+            Op::Insert(527, 0),
+            Op::Insert(6, 0),
+            Op::Delete(527),
+            Op::Insert(527, 0),
+            Op::Insert(6, 0),
+        ];
+        run_operations(operations);
+    }
+
+    #[test]
+    fn test_minimal_failing_input_4() {
+        let operations = vec![
+            Op::Insert(0, 0),
+            Op::Insert(0, 0),
+            Op::Insert(513, 0),
+            Op::Insert(0, 0),
+            Op::Insert(1, 0),
+            Op::Insert(2, 0),
+            Op::Insert(617, 0),
+            Op::Insert(618, 0),
+            Op::Insert(0, 0),
+            Op::Insert(0, 0),
+            Op::Insert(514, 0),
+            Op::Insert(467, 0),
+            Op::Insert(3, 0),
+            Op::Insert(4, 0),
+            Op::Insert(467, 0),
+            Op::Insert(512, 0),
+            Op::Insert(505, 0),
+            Op::Insert(5, 0),
+            Op::Delete(512),
+            Op::Delete(505),
         ];
         run_operations(operations);
     }
@@ -467,10 +760,11 @@ mod test {
         #[test]
         fn test_merkle_btree_txn_against_btreemap(operations in proptest::collection::vec(
             prop_oneof![
-                (any::<u32>(), any::<u32>()).prop_map(|(k, v)| Op::Insert(k, v)),
-                any::<u32>().prop_map(Op::Get),
+                (0..1000u32, 0..10u32).prop_map(|(k, v)| Op::Insert(k, v)),
+                (0..1000u32).prop_map(Op::Get),
+                (0..1000u32).prop_map(Op::Delete),
             ],
-            1..100
+            1..10_000
         )) {
             run_operations(operations);
         }
